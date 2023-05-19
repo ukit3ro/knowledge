c25ad20f3b907129e12ddf66080573e0a0fa2daa
ukit3ro <Crasher985@yandex.ru>
2023-05-19 15:20:37 +0500

    first commit

---
+++ python basics/if.py
@@ -0,0 +1,17 @@
+condition = 1
+another_condition = 12
+
+
+if condition:
+    pass
+#    block of instructions
+elif another_condition:
+    pass
+#    block of instructions2
+else:
+    pass
+#    alternative_instructions
+
+#inline if
+a = 0
+a = 5 if condition else -5

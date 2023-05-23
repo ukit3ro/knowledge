a7004f369d0ff087008945ee9d81780fffce8bcd
ukit3ro <Crasher985@yandex.ru>
2023-05-23 14:25:27 +0500

    ended main code.following commits with comms later

--- business/main.py
+++ business/main.py
@@ -1,31 +1,5 @@
 import json
-
-d = {
-    20: [
-        {
-            "name": "RUB",
-            "cost": 0.01354,
-            "amount": 1000
-        },
-                {
-            "name": "BTC",
-            "cost": 36000,
-            "amount": 0.03,
-        }
-    ],
-    21: [
-        {
-            "name": "RUB",
-            "cost": 0.01354,
-            "amount": 2000,
-        },
-                {
-            "name": "BTC",
-            "cost": 72000,
-            "amount": 0.03
-        }
-    ]    
-}
+from copy import deepcopy
 
 def save_data(data):
     with open("data.json", "w") as f:
@@ -36,4 +10,136 @@ def load_data():
         data = json.load(f)
     return data
 
-save_data(d)
+
+def show_percent(name, percent, width=50):
+    fill = "▊"
+    empty = "-"
+    bars = round(percent*width)
+    
+    progress = fill * bars + empty * (width-bars)
+    print(f'{name:10} - {round(percent*100)}% | {progress}')
+    
+
+def plot_stat(data, signs, height=12):
+    
+    for i in range(height):
+        if i % 3 == 0:
+            current = max(data) - (max(data) - min(data))/height*(i)
+            line = f"{str(round(current, 2)):10}"
+        else:
+            line = " " *10
+        for num in data:
+            if max(data) - (max(data) - min(data))/height*(i+1) <= num:
+                line += "▊   "
+            else:
+                line += "    "
+        print(line)
+    print(f"{str(round(min(data) ,2)):10}" + "▊   " * len(data))
+    print(" "*10 + "▊   " * len(data))
+    
+    line = " "*10
+    for sign in signs:
+        line += f"{sign:4}"
+    print(line)
+    
+    
+def show_convert(data):
+    weeks = list(sorted(data.keys()))
+    converted = []
+    for week in weeks:
+        currencies = data[week]
+        total = 0 
+        for curr in currencies:
+            total += float(curr["cost"]) * float(curr["amount"])
+        converted.append(round(total, 2))
+    plot_stat(converted, weeks)
+    
+def show_stat(data, weeknum):
+    total = 0
+    for curr in data[weeknum]:
+        total += float(curr["cost"]) * float(curr["amount"])
+        
+    stats = []
+    for curr in data[weeknum]:
+        stats.append( (curr["cost"] * curr["amount"] / total, curr["name"]) )
+    stats = reversed(sorted(stats))
+    for n, name in stats:
+        show_percent(name, n)
+
+
+def select_mode():
+    print()
+    print("-"*30)
+    print(' Выберите режим для работы:')
+    print(' 1. Добавить новую неделю')
+    print(' 2. Скопировать новую неделю')
+    print(' 3. Добавить информацию о валюте')
+    print(' 4. Изменить информацию о валюте')
+    print(' 5. Вывести статистику')
+    print(' 6. Вывести распределение валют')
+    print(' 7. Выйти из программы')
+    print("-"*30)
+    print()
+    return int(input())
+
+def loop():
+    data = load_data()
+    
+    while True:
+        mode = select_mode()
+        if mode == 1:
+            week = input(" Введите номер новой недели: ")
+            data[week] = []
+
+        elif mode == 2:
+            week = input(" Введите номер новой недели: ")
+            from_week = input(
+                " Введите номер недели, откуда будем копировать: ")
+            
+            data[week] = deepcopy(data[from_week])
+        elif mode == 3:
+            week = input(" Введите номер недели: ")
+            name = input(" Введите имя валюты: ")
+            cost = float(input(" Введите стоимость валюты:"))
+            amount = float(input(" Введите количество валюты: "))
+            data[week].append({
+                "name": name,
+                "cost": cost,
+                "amount": amount
+            })
+        elif mode == 4:
+            week  = input(" Введите номер недели: ")
+            name = input(" Введите имя валюты: ")
+            
+            num = None
+            for i, curr in enumerate(data[week]):
+                if curr["name"] == name:
+                    num = i
+            
+            if num == None:
+                print('Нет такой валюты!')
+            else:
+                curr_cost = data[week][num]["cost"]
+                cost = input(f" Введите стоимость валюты ({curr_cost}): ")
+                if cost:
+                    data[week][num]["cost"] = float(cost)
+                    
+                curr_amount = data[week][num]["amount"]
+                amount = input(f" Введите количество валюты ({curr_amount}): ")
+                if amount:
+                    data[week][num]["amount"] = float(amount)
+        
+        elif mode == 5:
+            show_convert(data)
+        
+        elif mode == 6:
+            week = input(" Введите номер недели:")
+            show_stat(data, week)
+            
+        elif mode == 7:
+            break
+        else:
+            print("Некорректный режимм!")
+    save_data(data)
+    
+loop()
